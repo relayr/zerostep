51d48bbf64394510ce1588fe33a712c73ffecf85
Wolfram Fischer <wolfram.fischer@relayr.de>
2017-08-11 11:56:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,203 @@
+'use strict'
+
+/**
+ * ZeroStep manages modules lifecycle
+ */
+class ZeroStep {
+  /**
+   * construct class
+   * @param {*} config Configuration for ZeroStep
+   */
+  constructor(config) {
+    const defaults = {
+      name: 'ZeroStep',
+      loggerCb: (name) => ({
+        info: (msg) => console.log(`${name}:- ${msg}`),
+        error: (msg) => console.error(`${name}:- ${msg}`),
+      }),
+    }
+
+    this._config = Object.assign(defaults, config)
+
+    this.name = this._config.name
+    this._logger = this._config.loggerCb(this._config.name)
+
+    this._modules = []
+
+    // Keep track of our services
+    this._services = new Map()
+    // Keep track of services which have been registered
+    this._registeredServices = new Map()
+    // TODO: How to prevent client to ask for destroy before init has settled?!?
+    this._initPromise = null
+    this._destroyPromise = null
+  }
+
+  /**
+   * Register a module
+   * modules will be initialized in the order in which they where registered, so order matters
+   * ZeroStep creates a shallow copy of originalModule
+   * @param {*} originalModule
+   */
+  register(originalModule) {
+    const module = Object.assign({}, originalModule)
+    // Basic sanity checks for modules order matters!
+    if (!module.hasOwnProperty('name')) {
+      throw new Error('Refusing to register module w/o name attribute')
+    }
+
+    if (!module.hasOwnProperty('init') && !(typeof module.init === 'function')) {
+      throw new Error(`Refusing to register module ${module.name} w/o init method`)
+    }
+
+    if (!module.hasOwnProperty('destroy') && !(typeof module.destroy === 'function')) {
+      throw new Error(`Refusing to register module ${module.name} w/o destroy method`)
+    }
+
+    if (module.hasOwnProperty('export') && !(typeof module.export === 'string')) {
+      throw new Error(`Refusing to register module ${module.name} which has a non string type export attribute`)
+    }
+
+    if (module.export) {
+      if (this._registeredServices.has(module.export)) {
+        throw new Error(
+          `Refusing to register service ${module.export} from module ${module.name} ` +
+          `but module ${this._registeredServices.get(module.export).name} registered it already`
+        )
+      } else {
+        this._registeredServices.set(module.export, module)
+      }
+    }
+
+    if (module.hasOwnProperty('imports') && !(Array.isArray(module.imports) && module.imports.every((imp) => typeof imp === 'string'))) {
+      throw new Error(`Refusing to register module ${module.name} which has a an imports attribute which is not a list of only strings`)
+    }
+
+    if (module.imports) {
+      const missingServices = module.imports.filter((imp) => !this._registeredServices.has(imp))
+      if (missingServices.length > 0) {
+        throw new Error(`Refusing to register module ${module.name} which wants to import missing services [${missingServices.join(', ')}]`)
+      }
+    }
+
+    this._modules.push(module)
+  }
+
+  /**
+   * Initialize registered modules in the order they where registered
+   * @return {Promise}
+   */
+  init() {
+    if (!this._initPromise) {
+      /* The following algorithm builds a promise chain recursively with all registered modules.
+       * Every module get's a context (ctx) with a logger and all imports.
+       * The tricky part is to gracefully handle exceptions.
+       * If you have modules a,b and c in that order and b throws an exception the exception should
+       * be reportet and a.destroy() must be called (because it already has been successfully initialized)
+       */
+      const buildInitChain = (promise, modules, undoList, moduleNames) => {
+        if (modules.length === 0) {
+          return promise.catch((err) => {
+            this._logger.error(`Could not initialize module ${moduleNames[undoList.length]}: ${err.message}, ${err.stack}`)
+            this._logger.error('Attempting to shutdown already initalized modules gracefully!')
+            this._shutDownModules(undoList.reverse())
+            throw err
+          })
+        } else {
+          const module = modules.shift()
+          moduleNames.push(module.name)
+          promise = promise.then(() => {
+            const ctx = {
+              logger: this._config.loggerCb(module.name),
+            }
+            if (module.imports) {
+              module.imports.forEach((imp) => ctx[imp] = this._services.get(imp))
+            }
+            const imports = module.imports ? module.imports.join(', ') : ''
+            const exports = module.export !== undefined ? module.export : ''
+            this._logger.info(`Initializing module ${module.name}(${imports}) -> [${exports}]`)
+            return module.init(ctx)
+          })
+          .then((ex) => {
+            if (module.export) {
+              if (ex !== undefined && ex != null) {
+                this._services.set(module.export, ex)
+              } else {
+                throw new Error(`Module ${module.name} broke contract and did not export service ${module.export}`)
+              }
+            }
+          })
+          .then(() => undoList.push(module))
+          return buildInitChain(promise, modules, undoList, moduleNames)
+        }
+      }
+
+      this._initPromise = buildInitChain(Promise.resolve(), this._modules.slice(), [], [])
+    }
+    return this._initPromise
+  }
+
+  /**
+   * Register global handlers for SIGINT, SIGTERM, SIGUSR2 to shut down this container and exit
+   * the process and call 'init' afterwards
+   * @return {Promise} @see init
+   */
+  initAsApplicationCore() {
+    this._logger.info('Registering global signal handlers (SIGINT, SIGTERM, SIGUSR2)')
+
+    // Handle Ctrl-C ...
+    process.on('SIGINT', () => {
+      this._logger.info('Received SIGINT...')
+      this.destroy().catch((err) => this._logger.error(err)).then(() => process.exit(0))
+    })
+
+    process.on('SIGTERM', () => {
+      this._logger.info('Received SIGTERM...')
+      this.destroy().catch((err) => this._logger.error(err)).then(() => process.exit(0))
+    })
+
+    // Take care of nodemon restart notification
+    // Nodemon expects us to send SIGUSR2 again once we handled our cleanup
+    // Do not exit - else nodemon won't restart the server w/o changes
+    process.once('SIGUSR2', () => {
+      this._logger.info('RECEIVED SIGUSR2...')
+      this.destroy().catch((err) => this._logger.error(err)).then(() => process.kill(process.pid, 'SIGUSR2'))
+    })
+    return this.init()
+  }
+
+  /**
+   * Destroy modules in the reversed order of their registration
+   * @return {Promise}
+   */
+  destroy() {
+    return this._shutDownModules(this._modules.reverse())
+  }
+
+  /**
+   * Shutdown the provided modules and log errors
+   * @param {*} modules
+   * @return {Promise}
+   */
+  _shutDownModules(modules) {
+    if (!this._destroyPromise) {
+      let destroyRunner = Promise.resolve()
+
+      modules.forEach((module) => {
+        destroyRunner = destroyRunner.then(() => {
+          this._logger.info(`Destroying module ${module.name}`)
+          return module.destroy()
+        }).catch((err) => {
+          this._logger.error(`Error destroying ${module.name}: ${err.message}`)
+          // No rethrow -> following modules might be able to shutdown in a clean way
+        })
+      })
+
+      this._destroyPromise = destroyRunner
+    }
+
+    return this._destroyPromise
+  }
+}
+
+module.exports = ZeroStep
---
+++ spec/zerostep.spec.js
@@ -0,0 +1,283 @@
+'use strict'
+
+const chai = require('chai')
+const sinon = require('sinon')
+
+const ZeroStep = require('../index')
+
+const expect = chai.expect
+
+const createDummyModule = () => {
+  return {
+    name: 'dummyModule',
+    init: () => Promise.resolve(),
+    destroy: () => Promise.resolve(),
+  }
+}
+
+const createFaultyInitModule = () => {
+  return {
+    name: 'Faulty init',
+    init: () => {
+      throw new Error('I am faulty!')
+    },
+    destroy: () => Promise.reject('Should not be possible to call me!'),
+  }
+}
+
+const createDefaultZeroStepConfig = () => ({
+  loggerCb: () => ({
+    info: () => true,
+    error: () => true,
+  }),
+})
+
+describe('canary test', function() {
+  it('should work', function(done) {
+    expect(true).to.equal(true)
+    done()
+  })
+})
+
+describe('ZeroStep class', () => {
+  let core = null
+
+  beforeEach(() => {
+    core = new ZeroStep(createDefaultZeroStepConfig())
+  })
+
+  afterEach(() => {
+    core = null
+  })
+
+  it('should be able to be instantiated', () => {
+    expect(core).to.exist
+  })
+
+  it('should have a register method', () => {
+    expect(core.register).to.exist
+  })
+
+  it('should have an init method', () => {
+    expect(core.init).to.exist
+  })
+
+  it('should have a destroy method', () => {
+    expect(core.destroy).to.exist
+  })
+})
+
+describe('ZeroStep interaction with modules', () => {
+  it('should be possible to register a module with core', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    core.register(createDummyModule())
+  })
+
+  it('should be an error to register a module w/o a name', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    expect(core.register.bind(core, ({}))).to.throw()
+  })
+
+  it('should be an error to register a module w/o an init method', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    expect(core.register.bind(core, ({name: 'module w/o init'}))).to.throw()
+  })
+
+  it('should be an error to register a module w/o a destroy method', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    expect(core.register.bind(core, ({name: 'module w/o destroy', init: () => true}))).to.throw()
+  })
+
+
+  it('should call init on a registered module when core.init() is called', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    const dummyModule = createDummyModule()
+    const stub = sinon.stub(dummyModule, 'init')
+    stub.resolves()
+
+    core.register(dummyModule)
+    return core.init().then(() => {
+      return expect(stub.calledOnce).to.be.true
+    })
+  })
+
+  it('should call init on two registered modules when core.init() is called', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    const dummyModule1 = createDummyModule()
+    const dummyModule2 = createDummyModule()
+    const init1 = sinon.stub(dummyModule1, 'init')
+    init1.resolves()
+    const init2 = sinon.stub(dummyModule2, 'init')
+    init2.resolves()
+
+
+    core.register(dummyModule1)
+    core.register(dummyModule2)
+
+    return core.init().then(() => {
+      expect(init1.calledOnce).to.be.true
+      expect(init2.calledOnce).to.be.true
+      expect(init1.calledBefore(init2)).to.be.true
+    })
+  })
+
+  it('should call init on two registered modules when core.init() is called', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    const dummyModule1 = createDummyModule()
+    const dummyModule2 = createDummyModule()
+    const dummyModule3 = createDummyModule()
+
+    const init1 = sinon.stub(dummyModule1, 'init')
+    init1.resolves()
+    const init2 = sinon.stub(dummyModule2, 'init')
+    init2.resolves()
+    const init3 = sinon.stub(dummyModule3, 'init')
+    init3.resolves()
+
+
+    core.register(dummyModule1)
+    core.register(dummyModule2)
+    core.register(dummyModule3)
+
+    return core.init().then(() => {
+      expect(init1.calledOnce).to.be.true
+      expect(init2.calledOnce).to.be.true
+      expect(init3.calledOnce).to.be.true
+      expect(init1.calledBefore(init2)).to.be.true
+    })
+  })
+
+  it('should call destroy on a registered module when core.destroy() is called', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    const dummyModule = createDummyModule()
+    const stub = sinon.stub(dummyModule, 'destroy')
+    stub.resolves()
+
+    core.register(dummyModule)
+    return core.init()
+      .then(() => core.destroy())
+      .then(() => {
+        return expect(stub.calledOnce).to.be.true
+      }
+    )
+  })
+
+  it('should call destroy on two registered modules when core.destroy() is called', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    const dummyModule1 = createDummyModule()
+    const dummyModule2 = createDummyModule()
+
+    const stub1 = sinon.stub(dummyModule1, 'destroy')
+    stub1.resolves()
+
+    const stub2 = sinon.stub(dummyModule2, 'destroy')
+    stub2.resolves()
+
+    core.register(dummyModule1)
+    core.register(dummyModule2)
+
+    return core.init()
+      .then(() => core.destroy())
+      .then(() => {
+        // Test for reverse order in destroy
+        expect(stub1.calledBefore(stub2)).to.be.false
+        expect(stub1.calledOnce).to.be.true
+        expect(stub2.calledOnce).to.be.true
+      }
+    )
+  })
+
+  it('should call destroy on three registered modules when core.destroy() is called', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    const dummyModule1 = createDummyModule()
+    const dummyModule2 = createDummyModule()
+    const dummyModule3 = createDummyModule()
+
+    const stub1 = sinon.stub(dummyModule1, 'destroy')
+    stub1.resolves()
+
+    const stub2 = sinon.stub(dummyModule2, 'destroy')
+    stub2.resolves()
+
+    const stub3 = sinon.stub(dummyModule3, 'destroy')
+    stub3.resolves()
+
+    core.register(dummyModule1)
+    core.register(dummyModule2)
+    core.register(dummyModule3)
+
+    return core.init()
+      .then(() => core.destroy())
+      .then(() => {
+        // Test for reverse order in destroy
+        expect(stub1.calledBefore(stub2)).to.be.false
+
+        expect(stub1.calledOnce).to.be.true
+        expect(stub2.calledOnce).to.be.true
+        expect(stub3.calledOnce).to.be.true
+      }
+    )
+  })
+
+
+  it('should call destroy on a registered module when core.init() encounters a module which throws an error during initialization', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    const dummyModule = createDummyModule()
+    const faultyInitModule = createFaultyInitModule()
+
+    const dummyInit = sinon.stub(dummyModule, 'init')
+    dummyInit.resolves()
+    const dummyDestroy = sinon.stub(dummyModule, 'destroy')
+    dummyDestroy.resolves()
+
+    const faultyInit = sinon.stub(faultyInitModule, 'init')
+    faultyInit.rejects(new Error('Faulty was here!'))
+    const faultyDestroy = sinon.stub(faultyInitModule, 'destroy')
+    faultyDestroy.resolves()
+
+
+    core.register(dummyModule)
+    core.register(faultyInitModule)
+
+    return core.init()
+      .catch((err) => true)
+      .then(() => {
+        expect(dummyInit.calledOnce).to.be.true
+        expect(dummyDestroy.calledOnce).to.be.true
+        expect(faultyInit.calledOnce).to.be.true
+        expect(faultyDestroy.calledOnce).to.be.false
+      }
+    )
+  })
+
+  it('should be possible to import a service from module1 in module2', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    const module1 = Object.assign(createDummyModule(), {export: 'module1-service', init: () => 'ice cream'})
+    const module2 = Object.assign(createDummyModule(), {
+      imports: ['module1-service'],
+      init: (ctx) => expect(ctx['module1-service']).to.equal('ice cream')}
+    )
+
+    core.register(module1)
+    core.register(module2)
+
+    return core.init()
+  })
+
+  it('should fail if a module breaks the contract and does not export something', () => {
+    const core = new ZeroStep(createDefaultZeroStepConfig())
+    const module1 = Object.assign(createDummyModule(), {export: 'module1-service', init: () => Promise.resolve()})
+
+    core.register(module1)
+
+    return core.init()
+      .then(() => {
+        throw new Error('Should never be here!')
+      })
+      .catch((err) => {
+        expect(err.message).to.equal('Module dummyModule broke contract and did not export service module1-service')
+      })
+  })
+})
+
+
